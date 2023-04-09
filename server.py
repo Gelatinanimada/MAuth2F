d4fe355df776a48ef1c30505535acbb42ad0f1be
Gelatinanimada <66890100+Gelatinanimada@users.noreply.github.com>
2023-04-09 18:02:07 -0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,12 @@
+import flask
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    print(request)
+    return "a"
+
+if __name__ == '__main__': 
+    app.run(host='127.0.0.1', port=443, debug=True, ssl_context="adhoc")
